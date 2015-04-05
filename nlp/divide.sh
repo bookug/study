ee3b7e0678c0f1f45984873f9ffdf2bd33ef3e62
syzz <1181955272@qq.com>
2015-04-05 21:19:59 +0800

    start to train

---
+++ nlp/divide.sh
@@ -0,0 +1,51 @@
+#! /bin/bash -
+#this script is used to create direectory for class, and divide essays into them accordingly
+#each class will have one folder named by its id, as well as each essay
+
+
+if [ $# -ne 2 ]
+then
+	echo "Invalid arguments list"
+	echo "USAGE: $0 file directory"
+fi
+
+sed -i '/<[ \t]*\/doc[ \t]*>/d' $1
+#TODO: remove spaces in quotes, or use two docid to match two lines
+while [ 1 ]
+do
+	str=`grep -m1 "<[ \t]*ccnc_cat" $1`
+	echo $str
+	if [ ! "$str" ]			# "$str" X$str = "X" -z $str
+	then
+		exit 0
+	fi
+	class=`echo $str | sed -e 's/<[^<>]*>//g' -e 's/[ \t]//g'`
+	echo $class
+	docid1=`grep -m1 "<[ \t]*doc" $1 | grep -o "\"[ \t0-9]*\"" | sed 's/[\"]//g'`
+	echo $docid1
+	docid2=`echo $str | grep -o "\"[ \t0-9]*\"" | sed 's/[\"]//g'`
+	echo $docid2
+	puredocid=`echo $docid2 | sed 's/[ \t]//g'`
+	if [ ! -e $2 ]
+	then
+		echo "$2 does not esist!"
+		exit 1
+	elif [ ! -d $2 ]
+	then
+		echo "$2 is not a directory!"
+		exit 1
+	fi
+
+	cd $2
+	if [ ! -e $class ]
+	then
+		mkdir $class
+	fi
+	cd ..
+	#use docid to locate
+	sed -n "/<[ \t]*doc id=\"[ \t]*$docid1[ \t]*\"[ \t]*>/,/<[ \t]*ccnc_cat id=\"[ \t]*$docid2[ \t]*\"[ \t]*>/p" $1 > "$puredocid".essay
+	sed -i "/<[ \t]*doc id=\"[ \t]*$docid1[ \t]*\"[ \t]*>/,/<[ \t]*ccnc_cat id=\"[ \t]*$docid2[ \t]*\"[ \t]*>/d" $1 
+	mv "$puredocid".essay "$2"/"$class"/
+done
+
+
--- nlp/extract.sh
+++ nlp/extract.sh
@@ -1,10 +1,52 @@
 #! /bin/bash -
-#this program is used to extract essay-class information, create
-#directory, remove tags and punctuation, then mv content to that directory
+#this program is used to remove tags and punctuation in essays in one given folder
+#then produce a list of 'words count' for each class
 
-#TODO: split firstly, create directory, remain only words
-sed -e '/<[ \t]*ccnc_cat/,/\/ccnc_cat[ \t]*>/d' -e 's/<[^>]*>//g' -e 's/[\[\]{}|\\\/!@#$%\^&\*\.\?-_=+,:;]//g' -e	's/["()（）《》【】？，•·。、…”“：‘；’]//g' train | less
-#~"'
-echo "Job Done!"
+function extract()
+{
+	`>words.set`
+	for file in `ls`
+	do
+		case $file in
+			[0-9]*.essay) 
+			sed -i -e '/<[ \t]*ccnc_cat/,/\/ccnc_cat[ \t]*>/d' -e 's/<[^>]*>//g' $file
+			sed -i -e 's/[[]{}|\\\/!@#$%\^&\*\.\?-_=+,:;]//g' $file 
+			sed -i -e 's/["()（）《》【】？，•·。、…”“：‘；’]//g' $file
+			cat $file >> words.set
+			echo "$file is extracted!";;
+			*);;
+		esac
+	done
+}
+
+DIR=$1
+if [ ! -e $DIR ]
+then
+	echo 'This directory does not exist!'
+	exit 1
+fi
+
+if [ -f $DIR ]
+then
+	echo 'It should not be a file!'
+	exit 1
+fi
+
+cd $DIR
+for dir in `ls`
+do
+	if [ -d $dir ]
+	then
+		cd $dir
+		extract
+		`>words.list`
+		#BETTER: IDF and relative frequency
+		awk '{for(i=1;i<=NF;i++) count[$i]++;};
+		END{
+		for(j in count) {print j,count[j] | "sort -r -n -k2";}
+		}' words.set | head -100 >> words.list
+		cd ..
+	fi
+done
 
 
