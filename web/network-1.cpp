8080cc8a1245dfc79521d8d514af0ade17e51054
syzz <1181955272@qq.com>
2015-05-19 11:12:19 +0800

    network

--- web/network-1.cpp
+++ web/network-1.cpp
@@ -27,17 +27,17 @@ class Buffer
 {
 public:
 	frame* fp;
-	int length;
+	unsigned length;
 	Buffer()
 	{
 		fp = NULL;
 		length = 0;
 	}
-	Buffer(frame* _fp, int _len)
+	Buffer(char* _fp, int _len)
 	{
-		length = _len;
+		length = (unsigned)_len;
 		fp = new frame;
-		*fp = *_fp;
+		*fp = *(frame*)_fp;
 	}
 };
 
@@ -108,7 +108,7 @@ public:
 		unsigned i = (head + offset) % MAX_QUEUE_LEN;
 		while(cnt > 0)
 		{
-			SendFRAMEPacket(this->q[i]->fp, this->q[i]->length);
+			SendFRAMEPacket((unsigned char*)this->q[i]->fp, this->q[i]->length);
 			i = (i + 1) % MAX_QUEUE_LEN;
 			cnt--;
 		}
@@ -128,25 +128,25 @@ int stud_slide_window_stop_and_wait(char *pBuffer, int bufferSize, UINT8 message
 		p = new Buffer(pBuffer, bufferSize);
 		queue.push(p);
 		if(queue.count() <= WINDOW_SIZE_STOP_WAIT)
-			SendFRAMEPacket(pBuffer, bufferSize);
+			SendFRAMEPacket((unsigned char*)pBuffer, (unsigned)bufferSize);
 		break;
 	case MSG_TYPE_RECEIVE:
 		tack = ((frame*)pBuffer)->head.ack;
-		if(ack == queue.top()->fp->head.seq)	//both are big-endian
+		if(tack == queue.top()->fp->head.seq)	//both are big-endian
 		{
 			queue.pop();
 			if(!queue.isEmpty())
 			{
 				p = queue.top();
-				SendFRAMEPacket(p->fp, p->length);
+				SendFRAMEPacket((unsigned char*)p->fp, p->length);
 			}
 		}
 		break;
-	case MSG_TYPE_TIME_OUT:
+	case MSG_TYPE_TIMEOUT:
 		tseq = *(UINT32*)pBuffer;
 		p = queue.top();
 		if(tseq == ntohl(p->fp->head.seq))
-			SendFRAMEPacket(p->fp, p->length);
+			SendFRAMEPacket((unsigned char*)p->fp, p->length);
 		break;
 	default:
 		return -1;
@@ -161,17 +161,27 @@ int stud_slide_window_back_n_frame(char *pBuffer, int bufferSize, UINT8 messageT
 {
 	unsigned tseq, tack;
 	Buffer* p;
+	int ret;
 	switch(messageType)
 	{
 	case MSG_TYPE_SEND:
 		p = new Buffer(pBuffer, bufferSize);
 		queue.push(p);
 		if(queue.count() <= WINDOW_SIZE_BACK_N_FRAME)
-			SendFRAMEPacket(pBuffer, bufferSize);
+			SendFRAMEPacket((unsigned char*)pBuffer, (unsigned)bufferSize);
 		break;
-	case MSG_TYPE_RECEIVE:
+	case MSG_TYPE_RECEIVE:	//QUERY: not on order, then? same question in next func
 		tack = ((frame*)pBuffer)->head.ack;
-		int ret = queue.find(tack, WINDOW_SIZE_BACK_N_FRAME);		//both are big-endian
+		if(tack == queue.top()->fp->head.seq)	//both are big-endian
+		{
+			queue.pop();
+			if(queue.count() >= WINDOW_SIZE_BACK_N_FRAME)
+			{
+				queue.send(WINDOW_SIZE_BACK_N_FRAME - 1, 1);
+			}
+		}
+		/*
+		ret = queue.find(tack, WINDOW_SIZE_BACK_N_FRAME);		//both are big-endian
 		if(ret >= 0)					//successfully found
 		{
 			unsigned t = queue.count(), i;
@@ -186,10 +196,11 @@ int stud_slide_window_back_n_frame(char *pBuffer, int bufferSize, UINT8 messageT
 				queue.send(WINDOW_SIZE_BACK_N_FRAME-num, t);
 			}
 		}
+		*/
 		break;
-	case MSG_TYPE_TIME_OUT:
+	case MSG_TYPE_TIMEOUT:
 		tseq = htonl(*(UINT32*)pBuffer);
-		int ret = queue.find(tseq, WINDOW_SIZE_BACK_N_FRAME);
+		ret = queue.find(tseq, WINDOW_SIZE_BACK_N_FRAME);
 		if(ret >= 0)
 		{
 			unsigned t = queue.count();
@@ -210,21 +221,32 @@ int stud_slide_window_back_n_frame(char *pBuffer, int bufferSize, UINT8 messageT
 int stud_slide_window_choice_frame_resend(char *pBuffer, int bufferSize, UINT8 messageType)
 {
 	unsigned tseq, tack;
+      unsigned tkind;	//frame_kind
 	Buffer* p;
+	int ret;
 	switch(messageType)
 	{
 	case MSG_TYPE_SEND:
 		p = new Buffer(pBuffer, bufferSize);
 		queue.push(p);
 		if(queue.count() <= WINDOW_SIZE_CHOICE_FRAME_RESEND)
-			SendFRAMEPacket(pBuffer, bufferSize);
+			SendFRAMEPacket((unsigned char*)pBuffer, (unsigned)bufferSize);
 		break;
 	case MSG_TYPE_RECEIVE:			
-		frame_kind kind = ntohl(((frame*)pBuffer)->head.kind);
-		if(kind == ack)
+		tkind = ntohl(((frame*)pBuffer)->head.kind);	
+		if(tkind == ack)
 		{
 			tack = ((frame*)pBuffer)->head.ack;
-			int ret = queue.find(ack, WINDOW_SIZE_CHOICE_FRAME_RESEND);
+			if(tack == queue.top()->fp->head.seq)	//both are big-endian
+			{
+				queue.pop();
+				if(queue.count() >= WINDOW_SIZE_CHOICE_FRAME_RESEND)
+				{
+					queue.send(WINDOW_SIZE_CHOICE_FRAME_RESEND - 1, 1);
+				}
+			}
+			/*
+			queue.find(tack, WINDOW_SIZE_CHOICE_FRAME_RESEND);
 			if(ret >= 0)
 			{
 				unsigned t = queue.count(), i;
@@ -239,11 +261,12 @@ int stud_slide_window_choice_frame_resend(char *pBuffer, int bufferSize, UINT8 m
 					queue.send(WINDOW_SIZE_BACK_N_FRAME-num, t);
 				}
 			}
+			*/
 		}
-		else if(kind == nak)
+		else if(tkind == nak)
 		{
 			tseq = ((frame*)pBuffer)->head.seq;
-			int ret = queue.find(tseq, WINDOW_SIZE_CHOICE_FRAME_RESEND);
+			ret = queue.find(tseq, WINDOW_SIZE_CHOICE_FRAME_RESEND);
 			queue.send(ret, 1);
 		}
 		break;
@@ -252,4 +275,3 @@ int stud_slide_window_choice_frame_resend(char *pBuffer, int bufferSize, UINT8 m
 	}
 	return 0;
 }
-
