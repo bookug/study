c1257f857f56f799b65c0ef72869f32eedeac1f9
qzxx-syzz <1181955272@qq.com>
2015-05-28 19:42:25 +0800

    Update network-2.cpp
    in software lab

--- web/network-2.cpp
+++ web/network-2.cpp
@@ -14,15 +14,28 @@ extern unsigned int getIpv4Address();
 
 // implemented by students
 
+unsigned short computeCheckSum(char* p, unsigned ihl)
+{
+	unsigned sum = 0, i;
+	ihl *= 2;
+	for(i = 0; i < ihl; ++i)
+		if(i != 5)				//not include checksum itself
+		{
+			sum += ntohs(*((unsigned short*)p + i));
+			sum %= 0xffff;		//in case of exceed
+		}
+	return 0xffff - (unsigned short)sum;	//~(unsigned short)sum
+}
+
 int stud_ip_recv(char *pBuffer,unsigned short length)
 {
 	unsigned short version, ihl, tlive, checksum;
-	unsigned addr, sum, i;			//dest address
+	unsigned addr;			//dest address
 
 	version = (*pBuffer) >> 4;
 	ihl = (*pBuffer) & 0xf;
 	tlive = *(pBuffer + 8);
-	checksum = ntohl(*(unsigned short*)(pBuffer + 10));
+	checksum = ntohs(*(unsigned short*)(pBuffer + 10));	//not use ntohl here
 	addr = ntohl(*((unsigned*)pBuffer + 4));
 
 	if(version != 4)
@@ -40,23 +53,15 @@ int stud_ip_recv(char *pBuffer,unsigned short length)
 		ip_DiscardPkt(pBuffer, STUD_IP_TEST_TTL_ERROR);
 		return 1;
 	}
-	if(address != getIpv4Address() && address != 0xffffffff)
+	if(addr != getIpv4Address() && addr != 0xffffffff)
 	{
 		ip_DiscardPkt(pBuffer, STUD_IP_TEST_DESTINATION_ERROR);
 		return 1;
 	}
 
-	sum = 0;
-	ihl *= 4;
-	for(i = 0; i < ihl; ++i)
+	if(computeCheckSum(pBuffer, ihl) != checksum)
 	{
-		sum += *(pBuffer + i);
-	}
-	for(i = 0; i < 2; ++i)
-		sum = (sum >> 16) + (sum & 0xffff);
-	if(sum != checksum)
-	{
-		ip_DiscardPkt(pBuffer, STUD_IP_TEST_TTL_ERROR);
+		ip_DiscardPkt(pBuffer, STUD_IP_TEST_CHECKSUM_ERROR);
 		return 1;
 	}
 
@@ -69,16 +74,22 @@ int stud_ip_Upsend(char *pBuffer,unsigned short len,unsigned int srcAddr,
 {
 	unsigned newlen = 5 * 4 + len;
 	char* newpBuffer = (char*)malloc(newlen);
+
 	memcpy(newpBuffer + 20, pBuffer, len);
-	*newpBuffer = (4 << 4) + 5;
-	srand(NULL);
+	*newpBuffer = (4 << 4) + 5;		//0x45
+	*((unsigned short*)newpBuffer + 1) = htons(newlen);
+
+	srand(0);
 	*((unsigned short*)(newpBuffer + 4)) = rand();
+
 	*(newpBuffer + 8) = ttl;
 	*(newpBuffer + 9) = protocol;
+
 	*((unsigned*)newpBuffer + 3) = htonl(srcAddr);
 	*((unsigned*)newpBuffer + 4) = htonl(dstAddr);
 
-	//TODO: compute the check-sum
+	unsigned short checksum = computeCheckSum(newpBuffer, 5);
+	*((unsigned short*)newpBuffer + 5) = htons(checksum);
 
 	ip_SendtoLower(newpBuffer, newlen);
 
